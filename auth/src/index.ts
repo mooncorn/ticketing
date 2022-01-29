214dd1372988f34034fcd344973cde1ced49afed
mooncorn <hidasior@gmail.com>
2022-01-29 00:05:38 -0500

    auth and infra setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000.');
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
 });
