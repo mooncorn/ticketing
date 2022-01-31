df9a9046eea15a09bb48c828489c12cbd9934cf5
mooncorn <hidasior@gmail.com>
2022-01-30 21:12:03 -0500

    signup tests created

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,25 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'FSDAHFJ';
+
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+
+  await mongoose.connect(uri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
