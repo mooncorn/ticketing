c9e165beade2049a5f218f3c1df653cd9158b415
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-09-25 19:14:39 -0400

    Configure NATS test listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -8,7 +8,15 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
-const options = stan.subscriptionOptions().setManualAckMode(true);
+const options = stan
+  .subscriptionOptions()
+  .setManualAckMode(true)
+  // Not ideal in the long run by itself because NATS
+  // resends all previous events to newly created services.
+  .setDeliverAllAvailable()
+  // NATS keeps track of all processed/unprocessed events by this service.
+  // It does not resend events that have already been processed by this service in the past.
+  .setDurableName('some-service');
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
@@ -20,7 +28,9 @@ stan.on('connect', () => {
 
   const subscription = stan.subscribe(
     'ticket:created',
-    'order-service-queue-group',
+    // A queue group name prevents the dump of history for a durable subscription
+    // if connection is lost. It also makes sure an event is sent to one instance of a service.
+    'service-queue-group-name',
     options
   );
 
