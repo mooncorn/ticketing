9b970350482cc7066de31cfd02846baf13ae5c41
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-09-25 20:00:16 -0400

    Extract listener logic into a class

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,61 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+export abstract class Listener {
+  /**
+   * Name of the event.
+   */
+  abstract subject: string;
+  /**
+   * A queue group name prevents the dump of history for a durable subscription
+   * if connection is lost. It also makes sure an event is sent to one instance of a service.
+   */
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions = () => {
+    return (
+      this.client
+        .subscriptionOptions()
+        // Not ideal in the long run by itself because NATS
+        // resends all previous events to newly created services.
+        .setDeliverAllAvailable()
+        .setManualAckMode(true)
+        .setAckWait(this.ackWait)
+        // NATS keeps track of all processed/unprocessed events by this service.
+        // It does not resend events that have already been processed by this service in the past.
+        .setDurableName(this.queueGroupName)
+    );
+  };
+
+  listen = () => {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  };
+
+  parseMessage = (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      return JSON.parse(data);
+    } else {
+      // handle buffer
+      JSON.parse(data.toString('utf8'));
+    }
+  };
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,50 +1,21 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TickedCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
-// stan is a client
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
-const options = stan
-  .subscriptionOptions()
-  .setManualAckMode(true)
-  // Not ideal in the long run by itself because NATS
-  // resends all previous events to newly created services.
-  .setDeliverAllAvailable()
-  // NATS keeps track of all processed/unprocessed events by this service.
-  // It does not resend events that have already been processed by this service in the past.
-  .setDurableName('some-service');
-
+const ticketCreatedListener = new TickedCreatedListener(stan);
 stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+  ticketCreatedListener.listen();
+});
 
-  stan.on('close', () => {
-    console.log('NATS connection closed');
-    process.exit();
-  });
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    // A queue group name prevents the dump of history for a durable subscription
-    // if connection is lost. It also makes sure an event is sent to one instance of a service.
-    'service-queue-group-name',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      const payload = JSON.parse(data);
-
-      console.log(`Received event #${msg.getSequence()}: with data:`, payload);
-    }
-
-    msg.ack();
-  });
+stan.on('close', () => {
+  console.log('NATS connection closed!');
+  process.exit();
 });
 
 // GRACEFUL CLIENT SHUTDOWN
